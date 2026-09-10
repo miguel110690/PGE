6a9f9a98384aa390ef255c6522d08ce0e35b8f5a
miguel110690 <miguel.guz.1106@gmail.com>
2026-09-10 17:12:18 -0500

    Update app2.py

--- app2.py
+++ app2.py
@@ -81,3 +81,67 @@ st.metric(
     "Total encargaturas vigentes",
     len(df_filtrado)
 )
+
+# --------------------------------------------------
+# FILTROS
+# --------------------------------------------------
+
+col1, col2 = st.columns(2)
+
+with col1:
+    filtro_procurador = st.text_input(
+        "🔍 Buscar Procurador"
+    )
+
+with col2:
+    filtro_entidad = st.text_input(
+        "🔍 Buscar Entidad (exacta)"
+    )
+
+# --------------------------------------------------
+# APLICAR FILTROS
+# --------------------------------------------------
+
+if filtro_procurador:
+
+    df_filtrado = df_filtrado[
+        df_filtrado["NOMBRE DEL PROCURADOR PÚBLICO"]
+        .astype(str)
+        .str.contains(
+            filtro_procurador,
+            case=False,
+            na=False
+        )
+    ]
+
+if filtro_entidad:
+
+    df_filtrado = df_filtrado[
+        df_filtrado["ENTIDAD"]
+        .astype(str)
+        .str.upper()
+        ==
+        filtro_entidad.upper()
+    ]
+
+# --------------------------------------------------
+# RENUMERAR FILAS
+# --------------------------------------------------
+
+df_filtrado = df_filtrado.reset_index(drop=True)
+
+df_filtrado.insert(
+    0,
+    "N°",
+    range(
+        1,
+        len(df_filtrado) + 1
+    )
+)
+
+# --------------------------------------------------
+# CONTADOR
+# --------------------------------------------------
+
+st.info(
+    f"Registros encontrados
